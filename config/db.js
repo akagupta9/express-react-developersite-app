41bf8465a66134ddd6d82f4a923465ab71a8ebf2
akagupta9 <akanksha.mitrc@gmail.com>
2021-02-03 19:01:10 +0530

    user registration

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const db = config.get("mongoURI");
 //Connection with mongo db
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useUnifiedTopology: true });
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    });
     console.log(`MongoDB database connection is successful.`);
   } catch (err) {
     console.log(`Error while connecting the Mongoo DB. ERROR: ${err}`);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,70 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const { check, validationResult } = require("express-validator/check");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const normalize = require("normalize-url");
 
 //@route  POST api/users
 //@desc  Register The user
 //@access public
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter Name").notEmpty(),
+
+    check("email", "Please enter valid email").isEmail(),
+
+    check("password", "Password length should be more than 6").isLength({
+      min: 6,
+      max: 20,
+    }),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //Validate user exists or not
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: { msg: "User Already Exists" } });
+      }
+
+      let avatar = normalize(
+        gravatar.url(email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        }),
+        { forceHttps: true }
+      );
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+    } catch (err) {
+      console.error(`error : ${err.message}`);
+      res.status(400).send("Server Error");
+    }
+
+    res.send("User Registered");
+  }
+);
 
 module.exports = router;
