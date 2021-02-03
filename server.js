6c5778d1ded7ce87b4afc60e1cb7775c0914d25a
akagupta9 <akanksha.mitrc@gmail.com>
2021-02-03 17:00:35 +0530

    script updated

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT ||5000;
+app.get("/", (req, res) => {
+  res.send("API is running ..");
+});
 
-app.listen(port,()=>{
-console.log(`Application is running on port : ${port}`)
-})
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Application is running on port : ${port}`);
+});
