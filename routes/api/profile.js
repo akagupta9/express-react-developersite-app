ecb77f10dce6d8fd440d80705715d0fd119cd9c3
akagupta9 <akanksha.mitrc@gmail.com>
2021-02-08 16:25:51 +0530

    mongo  url removed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-//@route  GET api/profile
-//@desc  Test The profile
-//@access public
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Profile Route");
+//@route  GET api/profile/me
+//@desc  get current users profile
+//@access private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for the user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Server Error", error: "Error :" + err });
+  }
 });
 
 module.exports = router;
